f44d3d964c62c981eb227263fe2eb591b5d24e92
Zalman <moishkugel@gmail.com>
2022-08-09 13:19:48 -0400

    commit tree 8c0cde61b65d3c8d8ef78f713f5287638b4c2912

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet <= 2000) {
+        return 'That will be twenty bucks.';
+    } else if (feet <= 2500) {
+          return 'I will gladly take your thirty bucks.';
+    } else {
+      return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message
+  switch (tip) {
+    case 'generous':
+      message =  'Thank you so much.';
+      break;
+    case 'not as generous' :
+      message =  'Thank you.';
+      break;
+    default:
+      message =  'Bye.';
+      break;   
+  }
+  return message;
 }
